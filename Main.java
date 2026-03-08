cb4c4bd33c68acdff39b683b73d74c5a2f3c3381
sathvikinguva <sathvikinguva@gmail.com>
2026-03-08 17:59:27 +0530

    Student Details Java Code

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Department: ");
+        String dept = sc.nextLine();
+
+        System.out.print("Enter Marks: ");
+        int marks = sc.nextInt();
+
+        System.out.println("\nStudent Details");
+        System.out.println("----------------");
+        System.out.println("Student ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Department: " + dept);
+        System.out.println("Marks: " + marks);
+    }
+}
